5ab1c310d0e556834efdd5ee3512f9e4c48f765e
core-coder101 <mohsinjawad86@gmail.com>
2024-06-05 15:37:49 +0500

    added basic sidebar base ;-;

---
+++ client/src/components/sidebar.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import "../css/Sidebar.css"
+
+function Sidebar() {
+    const [isOpen, setIsOpen] = useState(true)
+    
+    return(
+    <div className='Sidebar Open'>
+        
+    </div>
+    )
+}
+
+export default Sidebar
